f0d66ae61b77a3de0806e3398314bb0480103a36
JJ <jh_86@hotmail.co.uk>
2021-04-20 11:47:24 +0100

    counter problem sorted

--- src/Functions/Fight.js
+++ src/Functions/Fight.js
@@ -5,8 +5,8 @@ export function fight(oneName,hpOne,attOne,twoName,hpTwo,attTwo,setWinner, setWi
 
     let battleInfo= document.getElementById("battle-info");
 
-    let winCountPlus=winCount +1;
-    let winCountPlus2= winCountTwo +1;
+    let winCountPlus=winCount;
+    let winCountPlus2= winCountTwo;
 
   while(hpOne>0 && hpTwo>0){
       hpTwo=handleAttck(attOne, hpTwo,oneName,twoName);
@@ -14,14 +14,14 @@ export function fight(oneName,hpOne,attOne,twoName,hpTwo,attTwo,setWinner, setWi
       hpOne=handleAttck(attTwo,hpOne,twoName,oneName);    
     }
     if(hpOne<=0){
-      
+      winCountPlus2=winCountPlus2+1;
       console.log(`${oneName} fainted.`);
       console.log(`${twoName} won`);
       setWinner(twoName);
       setWinCountTwo(winCountPlus2)
       
     }else if(hpTwo<=0){
-       winCountPlus=winCountPlus++;
+       winCountPlus=winCountPlus+1;
       console.log(`${twoName} fainted`);
       console.log(`${oneName} won`);
       setWinner(oneName);
